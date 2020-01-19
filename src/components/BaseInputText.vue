d01f33cb73952d9e5f385338b0288b431db738bd
zhaoxiaomeng <zhaopku09@gmail.com>
2020-01-20 00:34:57 +0800

    Add Vue Router

---
+++ src/components/BaseInputText.vue
@@ -0,0 +1,40 @@
+<template>
+  <input
+    :value="value"
+    class="input"
+    type="text"
+    v-on="listeners"
+  >
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    listeners () {
+      return {
+        // Pass all component listeners directly to input
+        ...this.$listeners,
+        // Override input listener to work with v-model
+        input: event => this.$emit('input', event.target.value)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  $vue-blue: #32485F;
+  $vue-green: #00C185;
+
+  .input {
+    width: 100%;
+    padding: 8px 10px;
+    border: 1px solid $vue-blue;
+  }
+</style>
